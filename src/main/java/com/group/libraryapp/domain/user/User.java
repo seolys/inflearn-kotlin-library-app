44348384a3d0005102910c0b59bf95ebddb56b84
설연수 <seolnavy@naver.com>
2022-12-12 21:41:02 +0900

    유저 관련 기능 테스트 작성하기

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,67 +1,75 @@
 package com.group.libraryapp.domain.user;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 import com.group.libraryapp.domain.book.Book;
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
-
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @Entity
 public class User {
 
-  @Id
-  @GeneratedValue(strategy = IDENTITY)
-  private Long id;
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
 
-  @Column(nullable = false)
-  private String name;
+	@Column(nullable = false)
+	private String name;
 
-  private Integer age;
+	private Integer age;
 
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-  private final List<UserLoanHistory> userLoanHistories = new ArrayList<>();
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private final List<UserLoanHistory> userLoanHistories = new ArrayList<>();
 
-  public User() {
+	public User() {
 
-  }
+	}
 
-  public User(String name, Integer age) {
-    if (name.isBlank()) {
-      throw new IllegalArgumentException("이름은 비어 있을 수 없습니다");
-    }
-    this.name = name;
-    this.age = age;
-  }
+	public User(final String name, final Integer age) {
+		if (name.isBlank()) {
+			throw new IllegalArgumentException("이름은 비어 있을 수 없습니다");
+		}
+		this.name = name;
+		this.age = age;
+	}
 
-  public void updateName(String name) {
-    this.name = name;
-  }
+	public void updateName(final String name) {
+		this.name = name;
+	}
 
-  public void loanBook(Book book) {
-    this.userLoanHistories.add(new UserLoanHistory(this, book.getName(), false));
-  }
+	public void loanBook(final Book book) {
+		this.userLoanHistories.add(new UserLoanHistory(this, book.getName(), false));
+	}
 
-  public void returnBook(String bookName) {
-    UserLoanHistory targetHistory = this.userLoanHistories.stream()
-        .filter(history -> history.getBookName().equals(bookName))
-        .findFirst()
-        .orElseThrow();
-    targetHistory.doReturn();
-  }
+	public void returnBook(final String bookName) {
+		final UserLoanHistory targetHistory = this.userLoanHistories.stream()
+				.filter(history -> history.getBookName().equals(bookName))
+				.findFirst()
+				.orElseThrow();
+		targetHistory.doReturn();
+	}
 
-  public String getName() {
-    return name;
-  }
+	@NotNull
+	public String getName() {
+		return name;
+	}
 
-  public Integer getAge() {
-    return age;
-  }
+	@Nullable
+	public Integer getAge() {
+		return age;
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
 }
---
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -0,0 +1,90 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceTest @Autowired constructor(
+    private val userRepository: UserRepository,
+    private val userService: UserService
+) {
+
+    @AfterEach
+    fun clean() {
+        userRepository.deleteAll()
+    }
+
+    @Test
+    @DisplayName("유저 저장이 정상 동작한다")
+    fun saveUserTest() {
+        // given
+        val request = UserCreateRequest("seol", null)
+
+        // when
+        userService.saveUser(request)
+
+        // then
+        val users = userRepository.findAll()
+        assertThat(users).hasSize(1)
+        assertThat(users[0].name).isEqualTo("seol")
+        assertThat(users[0].age).isNull()
+    }
+
+    @Test
+    @DisplayName("유저 조회가 정상 동작한다")
+    fun getUsersTest() {
+        // given
+        userRepository.saveAll(
+            listOf(
+                User("A", 20),
+                User("B", null),
+            )
+        )
+
+        // when
+        val users = userService.getUsers()
+
+        // then
+        assertThat(users).hasSize(2)
+        assertThat(users).extracting("name").containsExactly("A", "B")
+        assertThat(users).extracting("age").containsExactly(20, null)
+    }
+
+    @Test
+    @DisplayName("유저 수정이 정상 동작한다")
+    fun updateUserNameTest() {
+        // given
+        val savedUser = userRepository.save(User("A", null))
+        val request = UserUpdateRequest(savedUser.id, "B")
+
+        // when
+        userService.updateUserName(request)
+
+        // then
+        val result = userRepository.findAll()[0]
+        assertThat(result.name).isEqualTo("B")
+    }
+
+    @Test
+    @DisplayName("유저 삭제가 정상 동작한다")
+    fun deleteUserTest() {
+        // given
+        val savedUser = userRepository.save(User("A", null))
+
+        // when
+        userService.deleteUser("A")
+
+        // then
+        assertThat(userRepository.findAll()).isEmpty()
+    }
+
+
+}
