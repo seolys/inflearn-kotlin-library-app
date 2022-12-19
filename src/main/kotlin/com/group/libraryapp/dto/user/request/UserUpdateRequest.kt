eb3ae6c4065d65a56aa680fa227ebafb4196047f
seolys <seolnavy@naver.com>
2022-12-19 18:52:04 +0900

    DTO를 Kotlin으로 변경하기

--- src/main/kotlin/com/group/libraryapp/dto/user/request/UserUpdateRequest.kt
+++ src/main/kotlin/com/group/libraryapp/dto/user/request/UserUpdateRequest.kt
@@ -1,21 +1,6 @@
-package com.group.libraryapp.dto.user.request;
+package com.group.libraryapp.dto.user.request
 
-public class UserUpdateRequest {
-
-	private long id;
-	private String name;
-
-	public UserUpdateRequest(final long id, final String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-}
+data class UserUpdateRequest(
+    val id: Long,
+    val name: String,
+)
--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -23,7 +23,7 @@ class UserService(
 
     @Transactional(readOnly = true)
     fun getUsers(): List<UserResponse> {
-        return userRepository.findAll().map(::UserResponse)
+        return userRepository.findAll().map(UserResponse::of)
     }
 
     @Transactional
